aecef89a0330c8c0dfdb12c38471a234b3140a28
aamanh30 <aamanh30@gmail.com>
2024-04-21 10:01:27 +0530

    feat(shared-ui) - full-width-layout.component.* - Added full width layout component to the library

---
+++ libs/shared/ui/src/lib/layouts/full-width-layout/full-width-layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-full-width-layout',
+  templateUrl: './full-width-layout.component.html',
+  styleUrl: './full-width-layout.component.scss',
+})
+export class FullWidthLayoutComponent {}
--- libs/shared/ui/src/lib/shared-ui.module.ts
+++ libs/shared/ui/src/lib/shared-ui.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoxedLayoutComponent } from './layouts/boxed-layout/boxed-layout.component';
+import { FullWidthLayoutComponent } from './layouts/full-width-layout/full-width-layout.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [BoxedLayoutComponent],
+  declarations: [BoxedLayoutComponent, FullWidthLayoutComponent],
 })
 export class SharedUiModule {}
