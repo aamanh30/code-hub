933ba2044aa8aff05eccf4f8246bcb2a45c057b8
aamanh30 <aamanh30@gmail.com>
2024-04-21 15:06:05 +0530

    refactor(progress-state) - progress-decorators, index - Created types for StartProgressDecorators and StopProgressDecorators

--- libs/progress/state/src/lib/models/progress-decorators.ts
+++ libs/progress/state/src/lib/models/progress-decorators.ts
@@ -1,8 +1,10 @@
 import { ProgressType } from './progress-type';
-import { StartProgress } from './start-progress';
 import { StopProgress } from './stop-progress';
 
-export type ProgressDecorators = { progressType: ProgressType } & (
-  | StartProgress
-  | StopProgress
-);
+export type StartProgressDecorators = {
+  progressType: ProgressType;
+};
+
+export type StopProgressDecorators = {
+  progressType: ProgressType;
+} & StopProgress;
