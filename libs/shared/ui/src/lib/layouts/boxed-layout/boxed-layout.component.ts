9e679bda16433b2a19ddf345d02881c8e5965675
aamanh30 <aamanh30@gmail.com>
2024-04-21 10:00:10 +0530

    feat(shared-ui) - boxed-layout.component.* - Added boxed layout component to the library

---
+++ libs/shared/ui/src/lib/layouts/boxed-layout/boxed-layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-boxed-layout',
+  templateUrl: './boxed-layout.component.html',
+  styleUrl: './boxed-layout.component.scss',
+})
+export class BoxedLayoutComponent {}
--- libs/shared/ui/src/lib/shared-ui.module.ts
+++ libs/shared/ui/src/lib/shared-ui.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BoxedLayoutComponent } from './layouts/boxed-layout/boxed-layout.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [BoxedLayoutComponent],
 })
 export class SharedUiModule {}
